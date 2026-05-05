fbd9f0f4199897cb931e8d29ba0d8f83bd9531ff
2benornot2ben <bk1@arizona.edu>
2026-05-05 10:30:00 +0800

    final upload for a bit

--- Main.java
+++ Main.java
@@ -61,6 +61,8 @@ public class Main {
 			return;
 		}
 		
+		// Read the file for the queries
+		
 		String line = "";
 		try {
 			while ((line = readr.readLine()) != null) {
@@ -124,6 +126,7 @@ public class Main {
 		
         
         while (true) {
+        	// This is the MAIN MENU. Not any SQL happening here.
         	ArrayList<String> inputList = new ArrayList<String>();
         	System.out.println("Please, first select method of usage:");
         	System.out.println("a : Admin. Get access to most direct-modification queries.");
@@ -131,7 +134,7 @@ public class Main {
         	System.out.println("c : Debug. We needed these for testing anyways.");
         	System.out.println("EXIT : End program.");
         	
-        	String[] nextLine = scanUse.nextLine().split(" ");
+        	String[] nextLine = convertToUsable(scanUse.nextLine());
         	if (nextLine.length == 0) continue;
         	
         	if (nextLine[0].toLowerCase().startsWith("a")) {
@@ -156,6 +159,19 @@ public class Main {
 	
 	public static int getInput(Scanner scanner, ArrayList<String> inputList, String[] responseString,
 			String... values) {
+		/* This is how input is gotten. This function is actually fairly complex!
+		 * The user has a lot of ways to deal with data.
+		 * They can put it all on the same line, in which it accepts it space-seperated (kind of)
+		 * They can also let it prompt them, which means one at a time.
+		 * Prompts are determines with the ... values!
+		 * Parameters: scanner, the basic input scanner.
+		 * inputList, the list to fill with data.
+		 * responseString, the overflow data from when this function was called.
+		 * It will take data from there first.
+		 * values, an unknown amount of strings which determines the prompts.
+		 * It also determines the amount of inputs it's looking for.
+		 * Returns: N/A (Modifies inputList)
+		 */
 		inputList.clear();
 		for (int index = 0; index < values.length; index++) {
 			if (responseString.length > index + 1) {
@@ -166,7 +182,7 @@ public class Main {
 				if (strVal.isEmpty()) index--;
 				else if (strVal.equalsIgnoreCase("Exit")) return 1;
 				else {
-					inputList.add(strVal.split(" ")[0]);
+					inputList.add(convertToUsable(strVal)[0]);
 				}
 			}
 		}
@@ -175,16 +191,23 @@ public class Main {
 	
 	public static void adminpanel(Scanner scanUse, ArrayList<String> inputList, Connection dbconn,
 			HashMap<String, String> queries) {
+		/* The admin panel!
+		 * Admin is not an account - it's closer to direct access.
+		 * This is also where the 4 required queries are, q1 to q4 respectively.
+		 * Parameters: scanUse, the input scanner
+		 * inputList, the arraylist to use for getting input (efficiency)
+		 * dbconn, the connection to the database.
+		 * queries, the hashmap from basic letter queries to the actual ran ones.
+		 * Returns: N/A (Prints)
+		 */
 		inputList.clear();
 		while (true) {
         	System.out.println("Admin Panel:");
         	System.out.println("a : Add User (Args: Like, a lot, idk)");
         	System.out.println("b : Update User (Args: Also like a lot, idk)");
         	System.out.println("c : Delete User (Args: 1)");
-        	System.out.println("d : Update Subscription of User (Args: 2)"); // What's a "record insertion
-        	// for a message"? It sounds just like an automatic check.
-        	System.out.println("e : Generate Invoice (Args: 1)"); // It's tier-based, so no 2nd arg
-        	// Where's mark as complete? That's on the user side.
+        	System.out.println("d : Update Subscription of User (Args: 2)");
+        	System.out.println("e : Generate Invoice (Args: 1)"); 
         	System.out.println("f : Assign Ticket (Args: 2)");
         	System.out.println("g : Modify Ticket (Args: 4)");
         	System.out.println("h : Modify Invoice (Args: 3)");
@@ -199,8 +222,10 @@ public class Main {
         	System.out.println("q7 : List all invoices.");
         	System.out.println("Logout : Return to main menu.");
         	
+        	// Modify determines which of the two query types is ran.
         	boolean modify = false;
-        	String[] nextLine = scanUse.nextLine().split(" ");
+        	// This is basically an advanced .split(" ")
+        	String[] nextLine = convertToUsable(scanUse.nextLine());
         	if (nextLine.length == 0) continue;
         	
         	if (nextLine[0].toLowerCase().equals("a")) {
@@ -298,8 +323,21 @@ public class Main {
 
 	public static void userpanel(Scanner scanUse, ArrayList<String> inputList, Connection dbconn,
 			HashMap<String, String> queries) {
+		/* The user panel!
+		 * Users have accounts! Therefore, you must log into one!
+		 * (Password is emitted because nobody cares, and I think it's not under doubt
+		 * that I can write the code for it if it was)
+		 * Users are a bit limited. Which makes sense, they're just users.
+		 * Parameters: scanUse, the input scanner
+		 * inputList, the arraylist to use for getting input (efficiency)
+		 * dbconn, the connection to the database.
+		 * queries, the hashmap from basic letter queries to the actual ran ones.
+		 * Returns: N/A (Prints)
+		 */
+		// In every query, arg 1 will be the userId.
 		int userId = queryLogIn(inputList, dbconn);
 		if (userId == -1) {
+			// Not an account? Then you cannot use this.
 			System.out.println("User not found!");
 			return;
 		}
@@ -307,7 +345,7 @@ public class Main {
 		inputList.clear();
 		while (true) {
         	System.out.println("Enter query for " + user);
-        	System.out.println("aa : Start Conversation (Args: 3, I think?)");
+        	System.out.println("aa : Start Conversation (Args: 3)");
         	System.out.println("ab : Add message to Conversation (Args: 2 (+1))");
         	System.out.println("ac : Update Message Feedback (Args: 4)");
         	System.out.println("ba : Move Conversation into Workspace (Args: 2)");
@@ -325,19 +363,20 @@ public class Main {
         	System.out.println("q1 : List conversations.");
         	System.out.println("q2 : List personas.");
         	System.out.println("q3 : List workspaces.");
+        	System.out.println("q4 : List messages in conversation (Args: 1)");
+        	System.out.println("q5 : Show review in conversation (Args: 1)");
         	
         	System.out.println("Logout : Return to main menu.");
         	
+        	// Modify determines which of the two query types is ran.
         	boolean modify = false;
-        	String[] nextLine = scanUse.nextLine().split(" ");
+        	// This is basically an advanced .split(" ")
+        	String[] nextLine = convertToUsable(scanUse.nextLine());
         	if (nextLine.length == 0) continue;
         	
         	if (nextLine[0].toLowerCase().equalsIgnoreCase("aa")) {
         		if (getInput(scanUse, inputList, nextLine, "Input Conversation Title: ",
         				"Input Persona: ", "Input Prompt: ") == 1) {
-        			// So, how do they input a persona and prompt?
-        			// No idea. Sorry. Maybe name and check if they have access?
-        			// Though that could have issues, too.
         			continue;
         		}
         		modify = true;
@@ -430,6 +469,16 @@ public class Main {
 
         	} else if (nextLine[0].toLowerCase().equalsIgnoreCase("q3")) {
 
+        	} else if (nextLine[0].toLowerCase().equalsIgnoreCase("q4")) {
+        		if (getInput(scanUse, inputList, nextLine, "Input Conversation Title: ") == 1) {
+        			continue;
+        		}
+        		
+        	} else if (nextLine[0].toLowerCase().equalsIgnoreCase("q5")) {
+        		if (getInput(scanUse, inputList, nextLine, "Input Conversation Title: ") == 1) {
+        			continue;
+        		}
+        		
         	} else if (nextLine[0].toLowerCase().equalsIgnoreCase("logout")) {
         		break;
         	} else {
@@ -443,14 +492,25 @@ public class Main {
 	
 	public static void extraspanel(Scanner scanUse, ArrayList<String> inputList, Connection dbconn,
 			HashMap<String, String> queries) {
+		/* The fake panel!
+		 * This is debug.
+		 * But, it felt cool to keep around, so I did.
+		 * Parameters: scanUse, the input scanner
+		 * inputList, the arraylist to use for getting input (efficiency)
+		 * dbconn, the connection to the database.
+		 * queries, the hashmap from basic letter queries to the actual ran ones.
+		 * Returns: N/A (Prints)
+		 */
 		inputList.clear();
 		while (true) {
         	System.out.println("Extras Panel:");
         	System.out.println("a : Print Tables");
         	System.out.println("Logout : Return to main menu.");
         	
+        	// Modify determines which of the two query types is ran.
         	boolean modify = false;
-        	String[] nextLine = scanUse.nextLine().split(" ");
+        	// This is basically an advanced .split(" ")
+        	String[] nextLine = convertToUsable(scanUse.nextLine());
         	if (nextLine.length == 0) continue;
         	
         	if (nextLine[0].toLowerCase().startsWith("a")) {
@@ -466,28 +526,64 @@ public class Main {
 	}
 	
 	private static void queryPlugin(ArrayList<String> iList, Connection dbconn, String query) {
-		/* Query is the formatted thingy
+		/* Runs the queries! Yes, all go to this one function!
+		 * Specifically, the SELECTS. Anything that needs to modify goes to the one below.
+		 * This function is extremely clever with how it handles things.
+		 * It can accept up to 10 args, or 9 if it's user (since one is prefilled)
+		 * How are the args used? The strings can have an "iList.get(n)" in them!
+		 * It will replace those with the user's args!
+		 * As for security, a user cannot have spaces in their args, so it cannot be breached...
+		 * ... Except for ones which are meant to be strings. It wouldn't make sense to limit those.
+		 * So, it accepts string queries with spaces.
+		 * Parameters: iList, the list of inputs, to which the fake gets pull from directly.
+		 * dbconn, the connection to the database.
+		 * Query, the query to run.
+		 * Returns: N/A (Prints)
 		 */
 		Statement stmt = null;
         ResultSet answer = null;
         
         String queryMod = query;
+        // Java in my SQL code? Well... Yes!
         int indexMod = queryMod.indexOf("iList.get(");
         	
+        // (While there's more...)
         while (indexMod != -1) {
-        	queryMod = queryMod.substring(0, indexMod) + iList.get(
-        			Integer.parseInt("" + queryMod.charAt(indexMod + 10)))
+        	// The users cannot write these parts, so we're safe.
+        	int val = Integer.parseInt("" + queryMod.charAt(indexMod + 10));
+        	String str = iList.get(val);
+        	// If there is a space...
+        	if (str.indexOf(" ") >= 0) {
+        		// Then is it a string query?
+        		int indexOne = indexMod - 1;
+        		int indexTwo = indexMod + 12;
+        		// (Two additional checks for OOB exceptions)
+        		if (indexOne < 0 || indexTwo >= queryMod.length()
+        			|| queryMod.charAt(indexOne) != '\''
+        			|| queryMod.charAt(indexTwo) != '\'') {
+        			// Technically speaking, you could use quotes for anything, as long as you don't
+        			// include spaces in them.
+        			// But they will be deleted.
+        			System.out.println("QUERY DENIED - Do not use quotes for non-strings!");
+        			return;
+        		}
+        	}
+        	// Replace it directly
+        	queryMod = queryMod.substring(0, indexMod) + str
         			+ queryMod.substring(indexMod + 12);
         	indexMod = queryMod.indexOf("iList.get(");
         }
         
 		try {
+			// Create and Execute
             stmt = dbconn.createStatement();
             answer = stmt.executeQuery(queryMod);
             if (answer != null) {
+            	// Grab metadata for column info
             	ResultSetMetaData deta = answer.getMetaData();
             	int columns = deta.getColumnCount();
             	while (answer.next()) {
+            		// Print
             		printLine(deta, answer, columns);
             	}
             }
@@ -506,25 +602,58 @@ public class Main {
 	}
 	
 	private static void queryModify(ArrayList<String> iList, Connection dbconn, String query) {
-		/* Query is the formatted thingy
-		 * 
+		/* Runs the modifying queries! Yes, all go to this one function!
+		 * Specifically, the non-SELECTS. Anything that needs to print goes to the one above.
+		 * This function is extremely clever with how it handles things.
+		 * It can accept up to 10 args, or 9 if it's user (since one is prefilled)
+		 * How are the args used? The strings can have an "iList.get(n)" in them!
+		 * It will replace those with the user's args!
+		 * As for security, a user cannot have spaces in their args, so it cannot be breached...
+		 * ... Except for ones which are meant to be strings. It wouldn't make sense to limit those.
+		 * So, it accepts string queries with spaces.
+		 * Parameters: iList, the list of inputs, to which the fake gets pull from directly.
+		 * dbconn, the connection to the database.
+		 * Query, the query to run.
+		 * Returns: N/A (Modifies DB)
 		 */
 		Statement stmt = null;
-        
-        String queryMod = query;
+		
+		String queryMod = query;
+        // Java in my SQL code? Well... Yes!
         int indexMod = queryMod.indexOf("iList.get(");
         	
+        // (While there's more...)
         while (indexMod != -1) {
-        	queryMod = queryMod.substring(0, indexMod) + iList.get(
-        			Integer.parseInt("" + queryMod.charAt(indexMod + 10)))
+        	// The users cannot write these parts, so we're safe.
+        	int val = Integer.parseInt("" + queryMod.charAt(indexMod + 10));
+        	String str = iList.get(val);
+        	// If there is a space...
+        	if (str.indexOf(" ") >= 0) {
+        		// Then is it a string query?
+        		int indexOne = indexMod - 1;
+        		int indexTwo = indexMod + 12;
+        		// (Two additional checks for OOB exceptions)
+        		if (indexOne < 0 || indexTwo >= queryMod.length()
+        			|| queryMod.charAt(indexOne) != '\''
+        			|| queryMod.charAt(indexTwo) != '\'') {
+        			// Technically speaking, you could use quotes for anything, as long as you don't
+        			// include spaces in them.
+        			// But they will be deleted.
+        			System.out.println("QUERY DENIED - Do not use quotes for non-strings!");
+        			return;
+        		}
+        	}
+        	// Replace it directly
+        	queryMod = queryMod.substring(0, indexMod) + str
         			+ queryMod.substring(indexMod + 12);
         	indexMod = queryMod.indexOf("iList.get(");
         }
         
 		try {
+			// Create and Update
             stmt = dbconn.createStatement();
             int val = stmt.executeUpdate(queryMod);
-            //dbconn.commit();
+            // Autocommit is on, so no need to commit.
             if (val != 0) System.out.println("Done! Affected " + val + " rows.");
             else System.out.println("Possible Fail: Affected 0 rows. May be due to "
             		+ "user error OR simply nothing being eligible.");
@@ -542,6 +671,16 @@ public class Main {
 	}
 	
 	private static int queryLogIn(ArrayList<String> inputList, Connection dbconn) {
+		/* A very specific query with a very specific job:
+		 * Logging the user in!
+		 * This is very similar to queryPlugin, except it has only one purpose,
+		 * and the only reason it is used is because it needs a return value.
+		 * The query is already known, so no query arg.
+		 * Returns -1 if one is not found.
+		 * Parameters: iList, the list of inputs, to which the fake gets pull from directly.
+		 * dbconn, the connection to the database.
+		 * Returns: int (userId or -1)
+		 */
 		Statement stmt = null;
         ResultSet answer = null;
         
@@ -549,9 +688,11 @@ public class Main {
         
 		try {
             stmt = dbconn.createStatement();
+            // Query is already known
             answer = stmt.executeQuery("SELECT userid FROM instuser WHERE username = '" + username + "'");
             if (answer != null) {
             	while (answer.next()) {
+            		// Return first. There should only be one, anyways.
             		return (answer.getInt(1));
             	}
             }
@@ -566,12 +707,22 @@ public class Main {
                 System.exit(-1);
 
         }
+		// Oops, no results!
 		return -1;
 	}
 	
 	private static void printLine(ResultSetMetaData mSet, ResultSet set, int columns) throws SQLException {
+		/* Prints out a line of query results!
+		 * Uses the column number and names from mSet to do so.
+		 * Uses a | Key: Value | Key: Value | format.
+		 * Parameters: mSet, which contains the metadata of the query.
+		 * set, which contains the non-meta data of the query.
+		 * columns, which is just gotten from mSet, but we avoid repeating ourselves w/ an arg.
+		 * Returns: N/A (Prints)
+		 */
 		StringBuilder buildor = new StringBuilder();
 		buildor.append(" | ");
+		// 1? Yes! Set is 1-indexed!
 		for (int i = 1; i <= columns; i++) {
 			buildor.append(mSet.getColumnName(i));
 			buildor.append(": ");
@@ -579,5 +730,35 @@ public class Main {
 			buildor.append(" | ");
 		}	
 		System.out.println(buildor.toString());
+	}
+	
+	private static String[] convertToUsable(String input) {
+		/* Advanced String.split(" ").
+		 * It works just like it, but it accepts ' and " as markers for "connected data".
+		 * While "in a quote", it won't split up the strings.
+		 * Note that, because of how it's made, "hi there' is perfectly okay.
+		 * Parameters: input, the input to split up.
+		 * Returns: String[], the split string.
+		 */
+		boolean inquotes = false;
+		StringBuilder buildor = new StringBuilder();
+		ArrayList<String> stringParts = new ArrayList<String>();
+		// Actually, a very simple method.
+		for (char c : input.toCharArray()) {
+			if (c == '\'' || c == '"') {
+				inquotes = !inquotes;
+			} else if (c == ' ' && !inquotes) {
+				// (If not empty)
+				if (buildor.length() > 0) {
+					stringParts.add(buildor.toString());
+					buildor.setLength(0);
+				}
+			} else {
+				buildor.append(c);
+			}
+		}
+		// Extra append since, what if it didn't end with a space?
+		if (buildor.length() > 0) stringParts.add(buildor.toString());
+		return stringParts.toArray(new String[0]);
 	}
 }
